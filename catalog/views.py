7fd079d21f8c3091761103059b39d75f8eaf55da
Clayton <sibandaclayton0@gmail.com>
2019-03-05 21:10:12 +0200

    added listview, detail view for books and authors

--- catalog/views.py
+++ catalog/views.py
@@ -25,6 +25,8 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
     num_genres_with_a = Genre.objects.filter(name__contains="fiction").count()
+    num_visits = request.session.get('num_visits',0)#get session value, if not available then set a value of 0 for it.
+    request.session['num_visits'] = num_visits+1
 
     context = {
         'num_books': num_books,
@@ -32,6 +34,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         "num_genres_with_a":num_genres_with_a,
+        'num_visits':num_visits,
     }
     return render(request,'index.html',context=context)
 
